f50b4355f91856f8f0e9574b9f5831ac2c4e8e20
Aurora Sky <62398803+melissa-clothilde@users.noreply.github.com>
2020-07-13 11:49:56 -0700

    implements previously searched cities sidebar and the local storage functionality in order to persist the cities searched

--- script.js
+++ script.js
@@ -1,28 +1,77 @@
 $(document).ready(function () {
-    var citySearched;
     var queryURLCurrent;
     var queryURLForecast;
     var forecastDate = $('.date');
     var searchBtn = $('#search-btn');
-    var APIKey = "69299ee3f1473733a3c1d646aa060339";
+    var APIKey = '69299ee3f1473733a3c1d646aa060339';
+    var body = $('.hide');
+    var cityList = document.getElementById('city-list');
+    var cityInput = document.getElementById("text-search");
+
+    var cities = [];
+    init();
+
+    function renderCities() {
+        // console.log(cities)
+        cityList.innerHTML = "";
+        // Render a new li for each city
+        for (var i = 0; i < cities.length; i++) {
+            var city = cities[i];
+
+            var li = document.createElement("li");
+            li.textContent = city;
+
+            li.setAttribute("data-index", i);
+
+            // var button = document.createElement("button");
+
+            li.setAttribute("type", "button");
+            // li.appendChild(button);
+            cityList.appendChild(li);
+        }
+    }
+
+    function init() {
+        // Get stored cities from localStorage
+        // Parsing the JSON string to an object
+        var storedCities = JSON.parse(localStorage.getItem("cities"));
 
 
+        if (storedCities !== null) {
+            cities = storedCities;
+        }
 
-    // Here, after the user clicks the search button, we run our AJAX calls with the Current and Forecast weather URLs to the OpenWeatherMap API
+        // Render cities to the DOM
+        renderCities();
+    }
+
+    function storeCities() {
+        localStorage.setItem("cities", JSON.stringify(cities));
+    }
 
     searchBtn.click(function () {
         event.preventDefault();
 
-        citySearched = $('#text-search').val();
+
+
+        var citySearched = cityInput.value.trim();
+
+        if (citySearched === "" || citySearched === null) {
+            return;
+        }
+
+
+        cities.push(citySearched);
+        cityInput.value = "";
+
 
         // Here we are building the URL we need to query the database for current weather
         queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearched + "&units=imperial&appid=" + APIKey;
 
         // Here we are building the URL we need to query the database for a 5 day forcast
         queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearched + "&units=imperial&appid=" + APIKey;
-        console.log('queryURLForecast', queryURLForecast);
+
         currentResponse();
-        console.log('citySearched', citySearched);
     })
 
     function currentResponse() {
@@ -38,10 +87,14 @@ $(document).ready(function () {
         $.ajax({
             url: queryURLCurrent,
             method: "GET"
+            ,
+            error: function () {
+                // if user puts in an invalid city
+                alert("Invalid city entered.");
+            }
         }).then(function (currentResponse) {
-
+            
             // Log the queryURL
-            console.log('currentResponse', currentResponse);
 
             var cityName = currentResponse.name;
             var currentIcon = currentResponse.weather[0].icon;
@@ -49,11 +102,11 @@ $(document).ready(function () {
             var currentTemperature = currentResponse.main.temp;
             var currentHumidity = currentResponse.main.humidity;
             var windSpeed = currentResponse.wind.speed;
-
+            body.removeClass('hide');
             $('.city-date').prepend(cityName);
 
             var m = moment();
-            console.log("moment", moment());
+
 
             var currentDate = m.format('L');
             $('.city-date').append(' (' + currentDate + ')');
@@ -61,26 +114,20 @@ $(document).ready(function () {
 
             $('#current-icon').attr('src', iconURL);
 
-            console.log(cityName);
-
-            console.log(currentTemperature);
             $('.current-temperature').append('Temperature: ', currentTemperature, ' \xB0', 'F');
 
 
-            console.log(currentHumidity);
 
             $('.current-humidity').append('Humidity: ', currentHumidity, '%');
 
-            console.log(windSpeed);
             $('.wind-speed').append('Wind Speed: ', windSpeed, ' MPH');
 
             // uv index api. color background based on the value. connect with lat and long. take lat and long in currentResponse, save it, then do the ajax call for the UV Index API
 
             var lat = currentResponse.coord.lat;
             var lon = currentResponse.coord.lon;
-            console.log("lat and lon", lat, lon);
 
-            queryURLUVI = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
+            queryURLUVI = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
             // function () {
             $.ajax({
                 url: queryURLUVI,
@@ -91,7 +138,6 @@ $(document).ready(function () {
                     uvIndex.empty();
                     $('.uv-index').append(response.value);
                     //look up .catch-- if it a failure, not success
-                    console.log(response);
 
                     var uvIndex = response.value
                     if (uvIndex < 2) {
@@ -115,7 +161,7 @@ $(document).ready(function () {
     function forecastResponse() {
         date = $('.date');
         date.empty();
-        fTemp = $('forecast-temperture');
+        fTemp = $('.forecast-temperture');
         fTemp.empty();
         fHumidity = $('.forecast-humidity')
         fHumidity.empty();
@@ -134,29 +180,38 @@ $(document).ready(function () {
                     dateStr = dateStr[0].split('-');
                     dateStr = dateStr[1] + '/' + dateStr[2] + '/' + dateStr[0];
                     $(this).append(dateStr);
-                    console.log('forecastDate.length', forecastDate.length);
-                        
+
+
                     var icon = $(this).siblings('img');
                     icon.attr('src', "http://openweathermap.org/img/wn/" + thisForecast.weather[0].icon + "@2x.png");
 
                     var temp = $(this).siblings('p:nth-child(3)');
                     var thisTemp = thisForecast.main.temp;
-                    temp.append(thisTemp, ' \xB0', 'F');
+                    temp.append('Temp: ' + thisTemp, ' \xB0', 'F');
 
                     var humidity = $(this).siblings('p:nth-child(4)');
                     var thisHumidity = thisForecast.main.humidity;
                     humidity.append('Humidity: ', thisHumidity, '%');
-
                 })
             })
-    storeCities();
+        if ()
+        storeCities();
+        renderCities();
     }
 
-    function storeCities () {
-        
-    }
+    // When a city is clicked...
+
+    cityList.addEventListener("click", function (event) {
+        body.addClass('hide');
+        var element = event.target;
+        var citySearched = element.textContent;
 
 
-})
+        queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearched + "&units=imperial&appid=" + APIKey;
 
+        queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearched + "&units=imperial&appid=" + APIKey;
 
+        currentResponse();
+    });
+
+});
